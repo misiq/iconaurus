cf1254ef58418f8953ca3e05b29fc7bbb0b84534
Michal Baran <michal.baran@live.com>
2023-02-19 22:13:41 +0100

    add sourcemap

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,19 +1,22 @@
 import { babel } from "@rollup/plugin-babel";
+import typescript from "@rollup/plugin-typescript";
 import filesize from "rollup-plugin-filesize";
 
 const config = {
-  input: "src/index.js",
+  input: "src/index.ts",
   output: {
-    file: "dist/index.esm.js",
+    file: "dist/index.esm.tsx",
     format: "esm",
+    sourcemap: true,
   },
   external: [/@babel\/runtime/, "react"],
+
   plugins: [
     babel({
       babelHelpers: "runtime",
       plugins: ["@babel/plugin-transform-runtime"],
     }),
-
+    typescript({ tsconfig: "./tsconfig.json", exclude: "react" }),
     filesize(),
   ],
 };
