6fc688d4b80c40c53c0d386f63a7fc2283aa7ec4
Michal Baran <michal.baran@live.com>
2023-02-19 22:25:38 +0100

    add types definitions

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,24 +1,31 @@
 import { babel } from "@rollup/plugin-babel";
 import typescript from "@rollup/plugin-typescript";
 import filesize from "rollup-plugin-filesize";
+import dts from "rollup-plugin-dts";
+const config = [
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.esm.tsx",
+      format: "esm",
+      sourcemap: true,
+    },
+    external: [/@babel\/runtime/, "react"],
 
-const config = {
-  input: "src/index.ts",
-  output: {
-    file: "dist/index.esm.tsx",
-    format: "esm",
-    sourcemap: true,
+    plugins: [
+      babel({
+        babelHelpers: "runtime",
+        plugins: ["@babel/plugin-transform-runtime"],
+      }),
+      typescript({ tsconfig: "./tsconfig.json", exclude: "react" }),
+      filesize(),
+    ],
   },
-  external: [/@babel\/runtime/, "react"],
-
-  plugins: [
-    babel({
-      babelHelpers: "runtime",
-      plugins: ["@babel/plugin-transform-runtime"],
-    }),
-    typescript({ tsconfig: "./tsconfig.json", exclude: "react" }),
-    filesize(),
-  ],
-};
+  {
+    input: "dist/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
 
 export default config;
